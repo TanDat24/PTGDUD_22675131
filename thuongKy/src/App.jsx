9d820563f3a91d3c75d505f828b99d31437f811d
TanDat24 <nguyentandat00522@gmail.com>
2025-04-14 12:03:35 +0700

    6_7. React Router and SPA single Application Page

--- thuongKy/src/App.jsx
+++ thuongKy/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 import Header from "./components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
+import Teams from "./pages/Team";
+import Analytics from "./pages/Analytics";
+import Messages from "./pages/Messages";
+import Integrations from "./pages/Integrations";
 
 function App() {
     return (
@@ -12,7 +17,17 @@ function App() {
                 <div className="flex-1">
                     <Header />
                     <main className="flex-1 p-8 bg-gray-50">
-                        <Dashboard />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/projects" element={<Projects />} />
+                            <Route path="/teams" element={<Teams />} />
+                            <Route path="/analytics" element={<Analytics />} />
+                            <Route path="/messages" element={<Messages />} />
+                            <Route
+                                path="/integrations"
+                                element={<Integrations />}
+                            />
+                        </Routes>
                     </main>
                 </div>
             </div>
